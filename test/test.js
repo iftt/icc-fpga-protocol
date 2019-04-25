5f20c856d5c8ea366df42f30539795e0ca40303b
CraigglesO <oconnorct1@gmail.com>
2019-04-25 01:39:13 -0400

    small test readme addition & travis fix

--- test/test.js
+++ test/test.js
@@ -1,6 +1,7 @@
 const test = require('tape')
 
-// NOTE: Test cases can only happen assuming an actual ICC-FPGA capable device is connected
+// NOTE: Test cases can only happen assuming an actual ICC-FPGA capable device is connected.catch
+// thus the user is responsible for proving tests are true
 
 test('timing test', function (t) {
   t.plan(1)
